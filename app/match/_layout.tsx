41f7b7e41f5bc511dbf837f37b581daf0ba8933d
Matheus Correia <matheuscorreiags@gmail.com>
2026-02-16 01:26:55 -0300

    refactor(match): remove MatchLayout and Match components

--- app/match/_layout.tsx
+++
@@ -1,5 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function MatchLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
--- app/match/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Match() {
-  return (
-    <View className="bg-background flex-1 items-center justify-center">
-      <Text className="text-white">Match page</Text>
-    </View>
-  );
-}
