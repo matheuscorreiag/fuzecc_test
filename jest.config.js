10f0644354e2bc41a55b6ae04bae78b8e245b942
Matheus Correia <matheuscorreiags@gmail.com>
2026-02-18 04:13:29 -0300

    tests(coverage): add coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,16 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
+  collectCoverageFrom: [
+    "hooks/**/*.ts",
+    "hooks/**/*.tsx",
+    "libs/**/*.ts",
+    "libs/**/*.tsx",
+    "components/**/*.ts",
+    "components/**/*.tsx",
+    "!**/*.d.ts",
+    "!**/__tests__/**",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
 };
